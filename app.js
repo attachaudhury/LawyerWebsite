7827e2119ceb450745409fc2b38c5f084b5901e4
Atta Rasul <attachaudhury@gmail.com>
2020-12-03 21:12:17 +0500

    changes

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 app.use(express.static("public"));
 
 
-// dbsetting();
+dbsetting();
 async function dbsetting() {
   await user.remove({});
   await contact.remove({});
